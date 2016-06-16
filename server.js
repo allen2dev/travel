f85642d228f9fd0773ffe4fe3acbeb8d61085e66
thanos <1770336162@qq.com>
2016-06-16 18:18:18 +0800

    add server route

--- server.js
+++ server.js
@@ -1,31 +1,75 @@
 'use strict';
 
+const Bcrypt = require('bcrypt');
 const Hapi = require('hapi');
+const Good = require('good');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.register(require('inert'), (err) => {
+const users = {
+    admin: {
+        username: 'admin',
+        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+        name: 'John Doe',
+        id: '2133d32a'
+    }
+};
 
-    if (err) {
-        throw err;
+const validate = function (request, username, password, callback) {
+    const user = users[username];
+    if (!user) {
+        return callback(null, false);
     }
 
-    server.route({
-        method: 'GET',
-        path: '/hello',
-        handler: function (request, reply) {
-            reply.file('./public/hello.html');
-        }
+    Bcrypt.compare(password, user.password, (err, isValid) => {
+        callback(err, isValid, { id: user.id, name: user.name });
     });
+};
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
 });
 
+server.route({
+    method: 'GET',
+    path: '/hello/{user?}',
+    handler: function (request, reply) {
+        const user = request.params.user ? encodeURIComponent(request.params.user) : 'stranger';
+        reply('Hello ' + user + '!');
+    }
+});
 
-
-server.start((err) => {
-
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info.uri);
-});
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        console.log('Server running at:', server.info.uri);
+    });
+})
