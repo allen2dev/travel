8aa2805ce48aa9e8198d4059da5a57c68420da77
thanos <1770336162@qq.com>
2016-06-20 17:51:04 +0800

    add template engine handlebars

--- server.js
+++ server.js
@@ -1,75 +1,34 @@
 'use strict';
-
-const Bcrypt = require('bcrypt');
 const Hapi = require('hapi');
-const Good = require('good');
+const Path = require('path');
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
 
-const users = {
-    admin: {
-        username: 'admin',
-        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
-        name: 'John Doe',
-        id: '2133d32a'
-    }
-};
+server.connection({
+    host: 'localhost',
+    port: '3000'
+});
+/*
+* 服务端路由
+* */
+server.register(require('vision'), () => {
+    server.views({
+        engines: {
+            hbs: require('handlebars')
+        },
+        relativeTo: __dirname,
+        layout: true,
+        path: 'views'
+    })
 
-const validate = function (request, username, password, callback) {
-    const user = users[username];
-    if (!user) {
-        return callback(null, false);
-    }
-
-    Bcrypt.compare(password, user.password, (err, isValid) => {
-        callback(err, isValid, { id: user.id, name: user.name });
-    });
-};
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
+    server.route({
+        method: ['POST', 'GET'],
+        path: '/',
+        handler: function (request, reply) {
+            reply.view('home')
+        }
+    })
 });
 
-server.route({
-    method: 'GET',
-    path: '/hello/{user?}',
-    handler: function (request, reply) {
-        const user = request.params.user ? encodeURIComponent(request.params.user) : 'stranger';
-        reply('Hello ' + user + '!');
-    }
-});
 
-server.register({
-    register: Good,
-    options: {
-        reporters: {
-            console: [{
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{
-                    response: '*',
-                    log: '*'
-                }]
-            }, {
-                module: 'good-console'
-            }, 'stdout']
-        }
-    }
-}, (err) => {
-    if (err) {
-        throw err;
-    }
-
-    server.start((err) => {
-
-        if (err) {
-            throw err;
-        }
-        console.log('Server running at:', server.info.uri);
-    });
-})
+server.start(() => console.log('Started at: ', server.info.uri));
